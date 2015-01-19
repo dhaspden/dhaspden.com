855b96a89cd7e12eea2972c6e7d2fe2e0fbefaec
Dylan Aspden <dhaspden@gmail.com>
2015-01-19 17:00:47 -0500

    Minify HTML files and put into public/dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
   
   var jsFiles = ['**/*.js', '!node_modules/**'];
+  var htmlFiles = ['public/src/views/*'];
 
   grunt.initConfig({
     package: grunt.file.readJSON('package.json'),
@@ -12,15 +13,35 @@ module.exports = function(grunt) {
       }
     },
 
+    copy: {
+      main: {
+        expand: true, cwd: 'public/src', src: '*', dest: 'public/dist/'
+      }
+    },
+
+    htmlmin: {
+      controlHtml: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {
+          'public/dist/views/index.min.html': 'public/src/views/index.html'
+        }
+      }
+    },
+
     watch: {
       scripts: {
-        files: jsFiles,
-        tasks: ['jshint']
+        files: [jsFiles, htmlFiles],
+        tasks: ['copy', 'htmlmin', 'jshint']
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
 };
