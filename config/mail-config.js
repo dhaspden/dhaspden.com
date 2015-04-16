95921ae115cb61d954ce0d8d16189f867051c202
Dylan Aspden <dhaspden@gmail.com>
2015-04-15 22:45:13 -0400

    Implement email capabilities on server

---
+++ config/mail-config.js
@@ -0,0 +1,23 @@
+var nodemailer = require('nodemailer');
+var account = require('./account.json');
+
+/* Sets up the service to send outgoing email messages */
+var transporter = nodemailer.createTransport({
+  service: account.service,
+  auth: {
+    user: account.username,
+    pass: account.password
+  }
+});
+
+/* Sends an email with the given parameters */
+transporter.sendEmail = function(email, subject, name, message) {
+  mailer.sendMail({
+    from: email,
+    to: account.username,
+    subject: subject,
+    text: name + ': ' + message
+  });
+}
+
+module.exports = transporter;
